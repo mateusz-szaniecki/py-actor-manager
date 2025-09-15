5641e95cd26910167c16f1d8c8eb4fdc4e6bafbe
Mateusz <szanieckimateusz@gmail.com>
2025-09-15 21:47:44 +0200

    Implemented Actor class

--- app/models.py
+++ app/models.py
@@ -1,4 +1,9 @@
 from dataclasses import dataclass
 
 
-# add dataclass here
+@dataclass
+class Actor:
+    def __init__(self, id: int, first_name: str, last_name: str) -> None:
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
